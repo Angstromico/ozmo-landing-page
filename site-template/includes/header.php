281e3ace31b2095c963c91338d844422bf03b173
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-11-12 10:17:45 -0400

    feat: No render with functions

--- site-template/includes/header.php
+++ site-template/includes/header.php
@@ -3,58 +3,6 @@ if (!defined('SITE_URL')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
-function render_contact_topbar($contacts) {
-    echo '<div class="topbar" style="flex-direction: column; gap: 5px; display: flex; margin-left: 55%;">';
-    
-    foreach ($contacts as $contact) {
-        $phone = $contact['phone'] ?? '';
-        $email = $contact['email'] ?? '';
-        
-        echo '<ul class="loginbar pull-right">';
-        
-        if ($phone) {
-            echo '<li style="font-size: 9px;">
-                    <i class="fa fa-phone highlight-color"></i>
-                    <a href="tel:' . htmlspecialchars($phone) . '">' . htmlspecialchars($phone) . '</a>
-                  </li>';
-        }
-        
-        if ($phone && $email) {
-            echo '<li class="hidden-xs topbar-devider"></li>';
-        }
-        
-        if ($email) {
-            echo '<li class="hidden-xs" style="font-size: 9px;">
-                    <i class="fa fa-envelope highlight-color"></i>
-                    <a href="mailto:' . htmlspecialchars($email) . '" style="font-size: 9px;">' . htmlspecialchars($email) . '</a>
-                  </li>';
-        }
-        
-        echo '</ul>';
-    }
-    
-    echo '</div>';
-}
-
-/**
- * Renders the main navigation menu from a simple associative array.
- * @param array $items The navigation items (Title => URL).
- */
-function render_nav_menu($items) {
-    // NOTE: In a real environment, this logic should check $_SERVER['REQUEST_URI']
-    // against the $url to dynamically set the 'active' class on the current page.
-    $current_path = '/'; 
-
-    foreach ($items as $title => $url) {
-        // Simple check: if URL is '/', mark as active.
-        $isActive = ($url == $current_path || ($title == 'Home' && $current_path == '/')) ? 'active' : '';
-        
-        echo '<li class="' . htmlspecialchars($isActive) . '">';
-        echo '  <a href="' . htmlspecialchars($url) . '">' . htmlspecialchars($title) . '</a>';
-        echo '</li>';
-    }
-}
-
 ?>
 
 <body class="dark">
@@ -69,25 +17,44 @@ function render_nav_menu($items) {
       <!-- End Logo -->
 
       <!-- Topbar -->
-       <?php
-        render_contact_topbar([
-          [
-            'phone' => PHONE_NUMBER1,
-            'email' => EMAIL_ADDRESS1
-          ],
-          [
-            'phone' => PHONE_NUMBER2,
-            'email' => EMAIL_ADDRESS2
-          ],
-          [
-            'phone' => PHONE_NUMBER3,
-            'email' => EMAIL_ADDRESS3
-          ]
-        ]);
-      ?>
+      <div class="topbar" style="flex-direction: column; gap: 5px; display: flex; margin-left: 55%;">
+        <?php
+        $contacts = [
+          ['phone' => PHONE_NUMBER1, 'email' => EMAIL_ADDRESS1],
+          ['phone' => PHONE_NUMBER2, 'email' => EMAIL_ADDRESS2],
+          ['phone' => PHONE_NUMBER3, 'email' => EMAIL_ADDRESS3]
+        ];
+
+        foreach ($contacts as $contact):
+          $phone = $contact['phone'] ?? '';
+          $email = $contact['email'] ?? '';
+        ?>
+          <ul class="loginbar pull-right">
+            <?php if ($phone): ?>
+              <li style="font-size: 9px;">
+                <i class="fa fa-phone highlight-color"></i>
+                <a href="tel:<?= htmlspecialchars($phone) ?>"><?= htmlspecialchars($phone) ?></a>
+              </li>
+            <?php endif; ?>
+
+            <?php if ($phone && $email): ?>
+              <li class="hidden-xs topbar-devider"></li>
+            <?php endif; ?>
+
+            <?php if ($email): ?>
+              <li class="hidden-xs" style="font-size: 9px;">
+                <i class="fa fa-envelope highlight-color"></i>
+                <a href="mailto:<?= htmlspecialchars($email) ?>" style="font-size: 9px;">
+                  <?= htmlspecialchars($email) ?>
+                </a>
+              </li>
+            <?php endif; ?>
+          </ul>
+        <?php endforeach; ?>
+      </div>
       <!-- End Topbar -->
 
-      <!-- Toggle get grouped for better mobile display -->
+      <!-- Toggle (mobile button) -->
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="fa fa-bars"></span>
@@ -96,17 +63,21 @@ function render_nav_menu($items) {
     </div>
     <!--/end container-->
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
+    <!-- Navigation Menu -->
     <div class="collapse navbar-collapse mega-menu navbar-responsive-collapse">
       <div class="container">
         <ul class="nav navbar-nav">
           <?php
-            // Renders the links defined in NAV_ITEMS dynamically
-            render_nav_menu(NAV_ITEMS); 
+          $current_path = '/';
+          foreach (NAV_ITEMS as $title => $url):
+            $isActive = ($url == $current_path || ($title == 'Home' && $current_path == '/')) ? 'active' : '';
           ?>
+            <li class="<?= htmlspecialchars($isActive) ?>">
+              <a href="<?= htmlspecialchars($url) ?>"><?= htmlspecialchars($title) ?></a>
+            </li>
+          <?php endforeach; ?>
         </ul>
       </div>
-      <!--/end container-->
     </div>
     <!--/navbar-collapse-->
   </div>
--- site-template/index.php
+++ site-template/index.php
@@ -2,69 +2,6 @@
 require_once __DIR__ . '/../config-tp15.php';
 include __DIR__ . '/includes/head.php';
 include __DIR__ . '/includes/header.php';
-
-function render_locations($locations) {
-    echo '<h2 style="text-align: center;">Our Locations</h2>';
-    echo '<div class="row margin-bottom-30 animated fadeInDown">';
-    
-    foreach ($locations as $loc) {
-        $icon = $loc['icon'] ?? 'fa-map-marker';
-        $name = $loc['name'] ?? 'Location';
-        $address = $loc['address'] ?? '';
-        $link = $loc['link'] ?? '#';
-        
-        echo '<a class="col-md-4" href="' . htmlspecialchars($link) . '" target="_blank">';
-        echo '  <div class="service">';
-        echo '    <i class="fa ' . htmlspecialchars($icon) . ' service-icon"></i>';
-        echo '    <div class="desc">';
-        echo '      <h4>' . htmlspecialchars($name) . '</h4>';
-        echo '      <p>' . nl2br(htmlspecialchars($address)) . '</p>';
-        echo '    </div>';
-        echo '  </div>';
-        echo '</a>';
-    }
-    
-    echo '</div>';
-}
-
-function render_star_rating($rating) {
-    $stars = '';
-    for ($i = 0; $i < 5; $i++) {
-        if ($i < $rating) {
-            $stars .= '<i class="fa fa-star" style="color: #f7b731;"></i>'; 
-        } else {
-            $stars .= '<i class="fa fa-star-o" style="color: #ccc;"></i>'; 
-        }
-    }
-    return $stars;
-}
-
-function render_testimonials($testimonials) {
-    echo '<div class="owl-ts-v1 owl-carousel owl-theme">'; 
-
-    foreach ($testimonials as $testimonial) {
-        $name = htmlspecialchars($testimonial['name']);
-        $location = htmlspecialchars($testimonial['location']);
-        $text = htmlspecialchars($testimonial['text']);
-        $car = htmlspecialchars($testimonial['car_purchased']);
-        $rating = (int)($testimonial['rating'] ?? 5);
-        $stars = render_star_rating($rating);
-
-        echo '<div class="item">';
-        echo '  <div class="testimonial-box" style="text-align: center; background: #fff; padding: 30px 40px; border-radius: 8px; border: 1px solid #eee; box-shadow: 0 5px 15px rgba(0,0,0,0.05);">';
-        echo '    <div class="testimonial-rating margin-bottom-10" style="font-size: 1.2rem;">' . $stars . '</div>';
-        echo '    <p style="font-style: italic; font-size: 1.1em; line-height: 1.6; color: #555; margin-bottom: 25px;">"' . $text . '"</p>';
-        echo '    <div class="testimonial-author">';
-        echo '      <strong style="display: block; font-size: 1.2em; color: #e9242d;">' . $name . '</strong>';
-        echo '      <span style="font-size: 0.9em; color: #888;">' . $location . ' | Purchased: ' . $car . '</span>';
-        echo '    </div>';
-        echo '  </div>';
-        echo '</div>';
-    }
-
-    echo '</div>';
-}
-
 ?>
 
 <!--=== Slider ===-->
@@ -76,137 +13,167 @@ function render_testimonials($testimonials) {
         <i><?php echo SLIDER_SUBTITLE_1; ?></i> <br />
         <i><?php echo SLIDER_SUBTITLE_1_2; ?></i>
       </h2>
-      <p>
-        <i><?php echo nl2br(SLIDER_DESCRIPTION_1); ?></i>
-      </p>
+      <p><i><?php echo nl2br(SLIDER_DESCRIPTION_1); ?></i></p>
       <div class="da-img">
         <img class="img-responsive" src="assets/plugins/parallax-slider/img/1.png" alt="" />
       </div>
     </div>
+
     <div class="da-slide da-slide-toleft">
       <h2>
         <i><?php echo SLIDER_TITLE_2; ?></i> <br />
         <i><?php echo SLIDER_SUBTITLE_2; ?></i> <br />
         <i><?php echo SLIDER_SUBTITLE_2_2; ?></i>
       </h2>
-      <p>
-        <i><?php echo nl2br(SLIDER_DESCRIPTION_2); ?></i>
-      </p>
+      <p><i><?php echo nl2br(SLIDER_DESCRIPTION_2); ?></i></p>
       <div class="da-img">
         <img class="img-responsive" src="assets/plugins/parallax-slider/img/2.png" alt="" />
       </div>
     </div>
+
     <div class="da-slide da-slide-fromright da-slide-current">
       <h2>
         <i><?php echo SLIDER_TITLE_3; ?></i> <br />
         <i><?php echo SLIDER_SUBTITLE_3; ?></i> <br />
         <i><?php echo SLIDER_SUBTITLE_3_2; ?></i>
       </h2>
-      <p>
-        <i><?php echo nl2br(SLIDER_DESCRIPTION_3); ?></i>
-      </p>
+      <p><i><?php echo nl2br(SLIDER_DESCRIPTION_3); ?></i></p>
       <div class="da-img">
         <img src="assets/plugins/parallax-slider/img/4.png" alt="image01" />
       </div>
     </div>
+
     <div class="da-arrows">
       <span class="da-arrows-prev"></span>
       <span class="da-arrows-next"></span>
     </div>
     <nav class="da-dots">
-      <span class=""></span><span class=""></span><span class="da-dots-current"></span>
+      <span></span><span></span><span class="da-dots-current"></span>
     </nav>
   </div>
 </div>
-<!--/slider-->
 <!--=== End Slider ===-->
 
 <!--=== Content Part ===-->
 <div class="container content-sm">
-  <!-- Directions Blocks -->
-   <?php
-    render_locations([
-      [
-        'icon'    => 'fa-tachometer',
-        'name'    => LOCATION_NAME1,
-        'address' => LOCATION_1_ADDRESS,
-        'link'    => MAP_LINK1
-      ],
-      [
-        'icon'    => 'fa-line-chart',
-        'name'    => LOCATION_NAME2,
-        'address' => LOCATION_2_ADDRESS,
-        'link'    => MAP_LINK2
-      ],
-      [
-        'icon'    => 'fa-bolt',
-        'name'    => LOCATION_NAME3,
-        'address' => LOCATION_3_ADDRESS,
-        'link'    => MAP_LINK3
-      ]
-    ]);
-  ?>
-  <!-- End Service Blokcs -->
+  <!-- Locations -->
+  <h2 style="text-align: center;">Our Locations</h2>
+  <div class="row margin-bottom-30 animated fadeInDown">
+    <?php
+    $locations = [
+      ['icon' => 'fa-tachometer', 'name' => LOCATION_NAME1, 'address' => LOCATION_1_ADDRESS, 'link' => MAP_LINK1],
+      ['icon' => 'fa-line-chart', 'name' => LOCATION_NAME2, 'address' => LOCATION_2_ADDRESS, 'link' => MAP_LINK2],
+      ['icon' => 'fa-bolt', 'name' => LOCATION_NAME3, 'address' => LOCATION_3_ADDRESS, 'link' => MAP_LINK3]
+    ];
 
-  <!-- Info Blokcs -->
+    foreach ($locations as $loc): ?>
+      <a class="col-md-4" href="<?= htmlspecialchars($loc['link']) ?>" target="_blank">
+        <div class="service">
+          <i class="fa <?= htmlspecialchars($loc['icon']) ?> service-icon"></i>
+          <div class="desc">
+            <h4><?= htmlspecialchars($loc['name']) ?></h4>
+            <p><?= nl2br(htmlspecialchars($loc['address'])) ?></p>
+          </div>
+        </div>
+      </a>
+    <?php endforeach; ?>
+  </div>
+  <!-- End Locations -->
+
+  <!-- Info Blocks -->
   <div class="row margin-bottom-30">
-    <!-- Welcome Block -->
-    <div class="col-md-7 md-margin-bottom-40 wow fadeInLeft" style="visibility: visible; animation-name: fadeInLeft">
+    <!-- Welcome -->
+    <div class="col-md-7 md-margin-bottom-40 wow fadeInLeft">
       <div class="skycontent">
         <div class="headline"><h2><?php echo WELCOME_TITLE; ?></h2></div>
-          <p><?php echo WELCOME_PARAGRAPH_1; ?></p>
-          <p><?php echo WELCOME_PARAGRAPH_2; ?></p>
-          <p><?php echo WELCOME_PARAGRAPH_3; ?></p>
-          <p><?php echo WELCOME_PARAGRAPH_4; ?></p>
+        <p><?php echo WELCOME_PARAGRAPH_1; ?></p>
+        <p><?php echo WELCOME_PARAGRAPH_2; ?></p>
+        <p><?php echo WELCOME_PARAGRAPH_3; ?></p>
+        <p><?php echo WELCOME_PARAGRAPH_4; ?></p>
       </div>
     </div>
 
-<!-- 3D Rotating Card Banner -->
-<div class="col-md-5 wow fadeInRight" style="visibility: visible; animation-name: fadeInRight">
-  <div class="headline"><h2>Special Offer</h2></div>
-  
-  <div class="card-3d-container">
-    <div class="card-3d">
-      <div class="card-front">
-        <div class="card-content">
-          <div class="card-icon">
-            <i class="fa fa-bolt" style="color: #e9242d; font-size: 3rem;"></i>
+    <!-- Offer Card -->
+    <div class="col-md-5 wow fadeInRight">
+      <div class="headline"><h2>Special Offer</h2></div>
+      <div class="card-3d-container">
+        <div class="card-3d">
+          <div class="card-front">
+            <div class="card-content">
+              <div class="card-icon">
+                <i class="fa fa-bolt" style="color: #e9242d; font-size: 3rem;"></i>
+              </div>
+              <h3>Performance Tuning</h3>
+              <p class="card-price">From £299</p>
+              <p class="card-desc">+30% HP & Torque<br>Improved Throttle Response<br>Better Fuel Economy</p>
+              <div class="card-badge">Limited Time</div>
+            </div>
           </div>
-          <h3>Performance Tuning</h3>
-          <p class="card-price">From £299</p>
-          <p class="card-desc">+30% HP & Torque<br>Improved Throttle Response<br>Better Fuel Economy</p>
-          <div class="card-badge">Limited Time</div>
-        </div>
-      </div>
-      <div class="card-back">
-        <div class="card-content">
-          <div class="card-icon">
-            <i class="fa fa-calendar-check-o" style="color: #e9242d; font-size: 3rem;"></i>
+          <div class="card-back">
+            <div class="card-content">
+              <div class="card-icon">
+                <i class="fa fa-calendar-check-o" style="color: #e9242d; font-size: 3rem;"></i>
+              </div>
+              <h3>Book Now</h3>
+              <p class="card-desc">Free Diagnostic Check<br>12 Month Warranty<br>Mobile Service Available</p>
+              <a href="<?php echo EXTERNAL_URL; ?>/contact" class="btn-u btn-u-red">Get Quote</a>
+            </div>
           </div>
-          <h3>Book Now</h3>
-          <p class="card-desc">Free Diagnostic Check<br>12 Month Warranty<br>Mobile Service Available</p>
-          <a href="<?php echo EXTERNAL_URL; ?>/contact" class="btn-u btn-u-red">Get Quote</a>
         </div>
       </div>
     </div>
   </div>
-</div>
 
-</div>
-</div>
-  <!-- End Info Blokcs -->
-
-  <!-- Owl Clients v1 -->
+  <!-- Testimonials -->
   <div class="headline"><h2><?php echo TESTIMONIALS_TITLE; ?></h2></div>
   <div class="margin-bottom-30">
-    <?php
-        render_testimonials(TESTIMONIALS); 
-    ?>
+    <div class="owl-ts-v1 owl-carousel owl-theme">
+      <?php foreach (TESTIMONIALS as $testimonial):
+        $rating = (int)($testimonial['rating'] ?? 5);
+      ?>
+      <div class="item">
+        <div class="testimonial-box" style="text-align: center; background: #fff; padding: 30px 40px; border-radius: 8px; border: 1px solid #eee; box-shadow: 0 5px 15px rgba(0,0,0,0.05);">
+          <div class="testimonial-rating margin-bottom-10" style="font-size: 1.2rem;">
+            <?php for ($i = 0; $i < 5; $i++): ?>
+              <i class="fa <?= $i < $rating ? 'fa-star' : 'fa-star-o' ?>" style="color: <?= $i < $rating ? '#f7b731' : '#ccc' ?>;"></i>
+            <?php endfor; ?>
+          </div>
+          <p style="font-style: italic; font-size: 1.1em; line-height: 1.6; color: #555; margin-bottom: 25px;">
+            "<?= htmlspecialchars($testimonial['text']) ?>"
+          </p>
+          <div class="testimonial-author">
+            <strong style="display: block; font-size: 1.2em; color: #e9242d;">
+              <?= htmlspecialchars($testimonial['name']) ?>
+            </strong>
+            <span style="font-size: 0.9em; color: #888;">
+              <?= htmlspecialchars($testimonial['location']) ?> | Purchased: <?= htmlspecialchars($testimonial['car_purchased']) ?>
+            </span>
+          </div>
+        </div>
+      </div>
+      <?php endforeach; ?>
+    </div>
   </div>
-  <!-- End Owl Clients v1 -->
 </div>
-<!--/container-->
+
+<!--=== Purchase Block ===-->
+<div class="purchase">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9 wow fadeInLeft">
+        <span><?php echo PURCHASE_TITLE; ?></span>
+        <p><?php echo PURCHASE_DESCRIPTION; ?></p>
+      </div>
+      <div class="col-md-3 btn-buy wow fadeInRight">
+        <a href="<?php echo CONTACT; ?>" class="btn-u btn-u-lg">
+          <i class="fa fa-edit"></i> <?php echo PURCHASE_BUTTON_TEXT; ?>
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
+<!--/row-->
+
 <script type="text/javascript" src="assets/js/app.js"></script>
 <script type="text/javascript" src="assets/js/plugins/owl-carousel.js"></script>
 <script type="text/javascript" src="assets/js/plugins/parallax-slider.js"></script>
@@ -217,24 +184,5 @@ function render_testimonials($testimonials) {
     ParallaxSlider.initParallaxSlider();
   });
 </script>
-<!-- End Content Part -->
 
-<!--=== Purchase Block ===-->
-<div class="purchase">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-9 wow fadeInLeft" style="visibility: hidden; animation-name: none">
-        <span><?php echo PURCHASE_TITLE; ?></span>
-        <p><?php echo PURCHASE_DESCRIPTION; ?></p>
-      </div>
-      <div class="col-md-3 btn-buy wow fadeInRight" style="visibility: hidden; animation-name: none">
-        <a href="<?php echo CONTACT ?>" class="btn-u btn-u-lg"><i class="fa fa-edit"></i> <?php echo PURCHASE_BUTTON_TEXT; ?></a>
-      </div>
-    </div>
-  </div>
-</div>
-<!--/row-->
-<!-- End Purchase Block -->
-<?php
-include __DIR__ . '/includes/footer.php';
-?>
+<?php include __DIR__ . '/includes/footer.php'; ?>
